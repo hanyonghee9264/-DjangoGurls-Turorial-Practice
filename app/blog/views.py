c6b1a66720870c5cfc4d6e2f4b7a80e83dcf2852
hanyonghee <hanyonghee9264@gmail.com>
2018-09-30 21:29:27 +0900

    post_list view에서 template을 loader를 통해 가져와 render한 결과를 리턴

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,33 +1,13 @@
 import os
 
 from django.http import HttpResponse
+from django.template import loader
 
 
 def post_list(request):
-    """
-    | (Root URL)에 해당하는 view
-    :param request:
-    :return:
-    """
-    view_file_path = os.path.abspath(__file__)
-    print(view_file_path)
-    blog_application_path = os.path.dirname(view_file_path)
-    print(blog_application_path)
-    app_dir = os.path.dirname(blog_application_path)
-    print(app_dir)
-    template_path = os.path.join(app_dir, 'templates', 'blog', 'post_list.html')
-    print(template_path)
-
-    # with문 사용
-    with open(template_path, 'rt') as f:
-        content = f.read()
-
-    # 파일객체 직접 사용 후 close
-    f = open(template_path, 'rt')
-    content = f.read()
-    f.close()
-
-    # 파일객체를 변수에 할당하지 않고 사용
-    content = open(template_path, 'rt').read()
-
-    return HttpResponse(content)
+    # 템플릿을 가져옴 (단순 문자열이 아님)
+    template = loader.get_template('blog/post_list.html')
+    # 해당 템플릿을 렌더링
+    context = {}
+    content = template.render(context, request)
+    return  HttpResponse(content)
