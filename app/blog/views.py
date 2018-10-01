982eb2a5ef7cdab036fb89cc6d079e4a9280dc1e
hanyonghee <hanyonghee9264@gmail.com>
2018-10-01 18:18:21 +0900

    post_list에서 post_detail로 이동하는 링크 구현

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,6 @@
+import re
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Post
@@ -9,3 +12,13 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post': post,
+    }
+    # templates/blog/post_detail.html을 Template으로 사용해서
+    #  post가 가진 title, text, author, created_date, published_date를 적절히 출력
+    return HttpResponse('Post Detail')
